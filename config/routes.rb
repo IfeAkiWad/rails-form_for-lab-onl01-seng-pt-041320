b46b09dc76b09f35314564fbf7fb18d6d91689bf
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-03 11:52:37 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :school_classes, only: [:new, :create, :edit, :show, :update]
+  get 'school_classes/:id/edit', to: 'school_classes#edit'
+  
+  resources :students, only: [:new, :create, :edit, :show, :update]
+  get 'students/:id/edit', to: 'students#edit'
 end
